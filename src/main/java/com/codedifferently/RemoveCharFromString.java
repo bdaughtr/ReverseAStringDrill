99102f355c20601bf806703bb65c4d380cf831ac
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-08-12 11:56:43 -0400

    non recursive

--- src/main/java/com/codedifferently/RemoveCharFromString.java
+++ src/main/java/com/codedifferently/RemoveCharFromString.java
@@ -1,13 +1,19 @@
 package com.codedifferently;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+//import com.sun.org.slf4j.internal.Logger;
+//import com.sun.org.slf4j.internal.LoggerFactory;
 
 public class RemoveCharFromString {
-    private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
+    //private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
 
     public String remove(String word, char unwanted){
-        return null;
+        String changed ="";
+
+        for(int i=0; i<word.length(); i++){
+            if(word.charAt(i) != unwanted)
+                changed += word.charAt(i);
+        }
+        return changed;
     }
 
     /*Extra Credit Flex*/
